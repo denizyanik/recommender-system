d982a2923c6ff416a4655ae7990fd5294074c4ef
dy1g18 <dy1g18@soton.ac.uk>
2021-04-28 18:59:14 +0100

    implemented deep learning keras model for recommender system

--- recommender.py
+++ recommender.py
@@ -2,10 +2,17 @@ import sqlite3
 
 import pandas
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
 np.random.seed(10)
+from keras.models import Model
+from keras.layers import Input, Reshape, Dot
+from keras.layers import Add, Activation, Lambda
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.regularizers import l2
 
-
+'''
 class RecommenderSystem:
 
     def __init__(self, k=40, gamma=0.001, lam=0, db=sqlite3.connect('example_table.db')):
@@ -56,7 +63,7 @@ class RecommenderSystem:
 
 
 recommender = RecommenderSystem()
-
+'''
 
 
 """
@@ -99,6 +106,65 @@ for i in range(0,n_iterations):
     print(q)
 """
 
+class RecommenderSystem:
+
+    def __init__(self,items,factors):
+        self.items = items
+        self.factors = factors
+
+    def __call__(self,x):
+        x = Embedding(self.items,self.factors,embeddings_initializer='he_normal',embeddings_regularizer=12(1e-6)) (x)
+        x = Reshape((self.n_factors,)) (x)
+        return x
+
+def recommender_system(n_users,n_items,n_factors,min_rating,max_rating,user,item):
+
+    u = RecommenderSystem(n_users,n_factors)(user)
+    ux = RecommenderSystem(n_users,1)(user)
+
+    i = RecommenderSystem(n_items, n_factors)(item)
+    ix = RecommenderSystem(n_items, 1)(item)
+
+    x = Dot(axes=1)([u,i])
+    x = Add()([x,ux,ix])
+    x = Activation('sigmoid')(x)
+    x = Lambda(lambda x: x * (max_rating - min_rating) + min_rating)(x)
+
+    model = Model(inputs=[user,item],outputs=x)
+    opt = Adam(lr=0.001)
+    model.compile(loss='mean_squared_error',optimizer = opt)
+
+    return model
 
 
+
+def process_data(data):
+    data = data[data['score'] != "rating"]
+
+    # encode user and movie fields to sequential integers
+    encoder = LabelEncoder()
+    data["user"] = encoder.fit_transform(data["userId"].values)
+    users = data["user"].nunique()
+
+    data["item"] = encoder.fit_transform(data["itemId"].values)
+    items = data["item"].nunique()
+
+    data["score"] = data["score"].values.astype(np.float32)
+    min_rating = min(data["score"])
+    max_rating = max(data["score"])
+
+    # create list of ((user,item),score) pairs
+    x = data[["user","item"]].values
+    y = data["score"].values
+
+    x = [x[:,0],x[:,1]]
+
+    return x,y
+
+
+train_small = pandas.read_csv(r"comp3208-train-small.csv",names = ["userId", "itemId","score","timestamp"])
+x_train, y_train = process_data(train_small)
+
+test_small = pandas.read_csv(r"comp3208-test-small.csv",names = ["userId", "itemId","score","timestamp"])
+x_test, y_test = process_data(test_small)
 
